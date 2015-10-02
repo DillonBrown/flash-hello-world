9c63140d6b27c05d06eb94052314434a868b4b32
DillonBrown <dbrown30130@gmail.com>
2015-10-02 13:49:11 -0400

    message about commit

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 @app.route("/hello")
 def hello_world():
 	return "Hello SFU World!"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
 if __name__ == "__main__":
 	app.run()
