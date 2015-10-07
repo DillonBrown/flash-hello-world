14fe2276a3e9d62b0c2f7eec97a69bb60624ffad
DillonBrown <dbrown30130@gmail.com>
2015-10-07 12:13:56 -0400

    Commiting some more stuffs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
+app.config("DEBUG") = TRUE
+
 @app.route("/")
 @app.route("/hello")
 def hello_world():
@@ -10,21 +12,27 @@ def hello_world():
 def search(search_query):
 	return search_query
 
-@app.route("/integer/<int:value>")
+@app.route("/int_test/<int:value>")
 def int_type(value):
-	print value+1
+	print (value+1)
 	return "correct"
 
-@app.route("/float/<float:value>")
+@app.route("/float_test/<float:value>")
 def float_type(value):
-	print value + 1
+	print (value+1)
 	return "correct"
 
-@app.route("/path/<path:value>")
+@app.route("/path_test/<path:value>")
 def path_type(value):
-	print value
+	print (value)
 	return "correct"
 
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "Francis":
+		return "Hello, {}".format(name)
+	else: 
+		return "Not Found", 404
 
 if __name__ == "__main__":
 	app.run()
